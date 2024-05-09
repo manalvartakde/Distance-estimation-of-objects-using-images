82dcb998129c2e4ba77a974d13348504fa607267
manalvartakde <manalvartakde@gmail.com>
2024-05-09 20:01:48 +0200

    function to augument the pixel coordinate added

--- dist_est-1.py
+++ dist_est-1.py
@@ -9,7 +9,8 @@ from pathlib import Path
 
 
 def read_data(folder_path):
-    """ Reads data and returs dictionares of calib and ground truth labels """
+    """Args: Folder path 
+    Returns: data in the form of dictionares of calib and ground truth labels """
     calib = {}
     labels = {}
     folders = ["calib", "labels"]
@@ -35,6 +36,9 @@ def read_data(folder_path):
 
 
 def detect_and_draw(image_path):
+    """Args: Image path
+    Detects cars in the image 
+    Returns image mat with bounding box around the cars and array with corner coordinates of bounding rectangle"""
     # YOLO v5 library path
     yolov5_dir = Path.home() / '.cache' / 'torch' / 'hub' / 'ultralytics_yolov5_master'
     sys.path.append(str(yolov5_dir))
@@ -44,22 +48,30 @@ def detect_and_draw(image_path):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.to(device).eval()
 
-    # Just make sure that the image in RGB
+    # Just makes sure that the image in RGB
     img = Image.open(image_path).convert("RGB")
-
     # model function used the pretrained yolo model to detect OBJECTS in the image
     results = model(img, size=640) # Image is resized to 640 x 640
 
     # Drawing bounding boxes around car
     image = cv2.imread(image_path)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    car_bounding_boxes = []
     for box in results.xyxy[0]:
         label = int(box[-1])
         if label == 2:  # here class value 2 is for cars..... we elimainte other detected objects
             xmin, ymin, xmax, ymax = map(int, box[:4])
+            car_bounding_boxes.append([xmin, ymin, xmax, ymax])
             cv2.rectangle(image_rgb, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+    # Display Images
+    image_rgb = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)
+    return(image_rgb, car_bounding_boxes)
 
-    # Display 
-    cv2.imshow("Detected Cars", cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+
+def augument_pixel(pixel_val):
+    """Args: Array of pixel coordinates[[x1, y1], [x2, y2]]
+    Returns: Augumented array [[x1, y1, 1], [x2, y2, 1]]"""
+    for coordinate in pixel_val:
+        coordinate.append(1)
+    return(pixel_val)
+
